391447cb4c5213d7d763b806ec453972869d9a8e
suspiciouslyBee <suspiciously@bee.movie>
2024-03-15 13:11:36 -0700

    Add project files.

---
+++ DailyCoding-1-702/main.h
@@ -0,0 +1,7 @@
+#pragma once
+////
+//	Author: suspiciouslyBee
+// 
+// 
+// 
+////
---
+++ DailyCoding-1-702/node.h
@@ -0,0 +1,18 @@
+#pragma once
+////
+//	Author: suspiciouslyBee
+// 
+//	File: Node Header
+//	
+//	Implementing the pseudocode into C++:
+/*
+class Node :
+def __init__(self, val, left = None, right = None) :
+	self.val = val
+	self.left = left
+	self.right = right
+*/
+// 
+////
+
+class Node
