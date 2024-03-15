7ba65335c97581c4a322c24f530b4ab965041fb9
suspiciouslyBee <suspiciously@bee.movie>
2024-03-15 14:07:07 -0700

    main drafted

--- DailyCoding-1-702/main.cpp
+++ DailyCoding-1-702/main.cpp
@@ -16,9 +16,14 @@ using namespace std;
 int main(void) {
 	string input = "6 9 3 5 5 7 2 10 4 1 8";
 	Tree tree;
+	string output;
+
 	cout << " Reading into tree\n";
 	cout << "Input list: " << input << endl;
-
+	tree.deserialize(input);
+	cout << "Serializeing...\n";
+	output = tree.serialize();
+	cout << (output == input) << endl;
 
 
 	return 0;
